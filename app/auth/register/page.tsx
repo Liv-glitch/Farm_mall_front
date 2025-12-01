c342ba2edf15805436a07168f2abe3286928ca98
Andrew Charles Ndavu <89539814+Ndavucha@users.noreply.github.com>
2025-12-02 00:10:13 +0300

    Update page.tsx

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -14,7 +14,7 @@ import { Eye, EyeOff, ArrowLeft } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
-import { useAuth } from "@/lib/hooks/use-auth"
+//import { useAuth } from "@/lib/hooks/use-auth"
 
 const kenyanCounties = [
   "Baringo",
